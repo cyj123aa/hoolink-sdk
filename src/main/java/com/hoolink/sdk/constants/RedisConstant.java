dd5738976c3804575e2568d26b4cfaeec38e684d
陈志镖 <1193381165@qq.com>
2019-05-10 14:00:52 +0800

    需求_594,任务_5662:升级参数

---
+++ src/main/java/com/hoolink/sdk/constants/RedisConstant.java
@@ -0,0 +1,16 @@
+package com.hoolink.sdk.constants;
+
+/**
+ * @author : chenzb
+ * @Description : Redis相关常量
+ * @date : Created on 2019/5/10 13:29
+ */
+public class RedisConstant {
+
+    /**
+     * 远程升级和强制回滚
+     */
+    public static final String DEVICE_UPGRADE_START = "device_upgrade_start";
+    public static final String DEVICE_UPGRADE_END = "device_upgrade_end";
+    public static final String DEVICE_UPGRADE_All = "device_upgrade_all";
+}
