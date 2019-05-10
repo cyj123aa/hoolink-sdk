8a9d3bc3c2e1ebd1cd70e6bf47891f6ebccce228
陈岳军 <491199291@qq.com>
2019-05-10 15:47:17 +0800

    设备上行

---
+++ src/main/java/com/hoolink/sdk/enums/command/UpgradeStateEnum.java
@@ -0,0 +1,36 @@
+package com.hoolink.sdk.enums.command;
+
+/**
+ * @author chenyuejun
+ */
+
+public enum UpgradeStateEnum {
+    /**
+     *发送失败
+     */
+    SEND_ERROR,
+    /**
+     *发送成功
+     */
+    SEND_SUCCESS,
+    /**
+     *接包失败
+     */
+    SEND_PACKAGE_ERROR,
+    /**
+     *接包成功
+     */
+    SEND_PACKAGE_SUCCESS,
+    /**
+     *升级失败
+     */
+    UPGRADE_ERROR,
+    /**
+     *升级成功
+     */
+    UPGRADE_SUCCESS
+
+
+
+
+}
