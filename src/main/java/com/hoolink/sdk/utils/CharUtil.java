ce37513663953892b088d2ff07a65f61260fff53
许保锋 <yanwu0527@163.com>
2019-05-15 16:39:42 +0800

    提交: 保证序列号中必须有字符

--- src/main/java/com/hoolink/sdk/utils/CharUtil.java
+++ src/main/java/com/hoolink/sdk/utils/CharUtil.java
@@ -11,8 +11,8 @@ import java.util.Random;
 public class CharUtil {
 
     private static final char[] CHARS = {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     };
 
     /**
@@ -35,6 +35,6 @@ public class CharUtil {
      * @return
      */
     public static char getRandomChar() {
-        return CHARS[new Random().nextInt(36)];
+        return CHARS[new Random().nextInt(26)];
     }
 }
