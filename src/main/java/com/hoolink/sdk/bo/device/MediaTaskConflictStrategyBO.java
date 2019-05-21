fef1906ab6ee50b970ad17a258660e5c8e154d0d
胡亚曦 <hyx199170@163.com>
2019-05-21 13:08:21 +0800

    提交数字音响BO

---
+++ src/main/java/com/hoolink/sdk/bo/device/MediaTaskConflictStrategyBO.java
@@ -0,0 +1,14 @@
+package com.hoolink.sdk.bo.device;
+
+import lombok.Data;
+
+/**
+ * @ClassName MediaTaskConflictStrategyBO
+ * @Author huyaxi
+ * @DATE 2019/5/20 9:44
+ */
+@Data
+public class MediaTaskConflictStrategyBO {
+    private String strategy;
+    private Boolean conflict;
+}
