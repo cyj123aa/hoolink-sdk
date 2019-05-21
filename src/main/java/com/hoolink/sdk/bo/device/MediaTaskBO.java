6f5cb478d6d9acc696607b745aa968419beaa454
hoolink_zhouyun <492725557@qq.com>
2019-05-21 16:35:41 +0800

    MediaTaskBO

--- src/main/java/com/hoolink/sdk/bo/device/MediaTaskBO.java
+++ src/main/java/com/hoolink/sdk/bo/device/MediaTaskBO.java
@@ -117,12 +117,6 @@ public class MediaTaskBO implements Serializable {
     private String random;
 
     /**
-     * 三方任务ID
-     * 表字段 : media_task.random
-     */
-    private String random;
-
-    /**
      * 策略列表：周或者日期
      */
     private List<TaskStrategyBO> strategys;
