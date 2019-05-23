d263303ee2c8739c7abd1a1376f165bb2fafa198
王栋 <305306733@qq.com>
2019-05-23 20:54:57 +0800

    需求_616_任务_6265:新增显示屏文件批量删除BO

---
+++ src/main/java/com/hoolink/sdk/bo/device/ScreenShuttleBoxBO.java
@@ -0,0 +1,23 @@
+package com.hoolink.sdk.bo.device;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author wangdong
+ * @date 2019/5/23 20:42
+ */
+@Data
+public class ScreenShuttleBoxBO implements Serializable {
+    private static final long serialVersionUID = -8442680451501821327L;
+
+    /*** 全部显示屏 */
+    private List<ScreenShuttleBoxBasicBO> allScreen;
+
+    /*** 已被选择的显示屏 */
+    private List<ScreenShuttleBoxBasicBO> beSelectScreen;
+
+
+}
