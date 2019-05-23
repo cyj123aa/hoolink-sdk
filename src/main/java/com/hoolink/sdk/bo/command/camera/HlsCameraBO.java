666e55953f32b977c7ede967946fbbe851808384
陈岳军 <491199291@qq.com>
2019-05-23 13:22:12 +0800

    Merge branch 'test' of 192.168.1.172:hoolink/hoolink-sdk into bug-fix
    # Conflicts:
    #	src/main/java/com/hoolink/sdk/bo/manager/DeviceUpgradeBO.java

--- src/main/java/com/hoolink/sdk/bo/command/camera/HlsCameraBO.java
+++ src/main/java/com/hoolink/sdk/bo/command/camera/HlsCameraBO.java
@@ -1,56 +1,21 @@
 package com.hoolink.sdk.bo.command.camera;
 
+import lombok.Data;
+
 /**
  * @author chenzhixiong
  * @date 2018/12/29 14:02
  */
+@Data
 public class HlsCameraBO {
     private Long projectId;
     private Integer pageSize;
     private Integer pageNo;
 
-    private String searchCode;
-
-    public Integer getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public Integer getPageNo() {
-        return pageNo;
-    }
-
-    public void setPageNo(Integer pageNo) {
-        this.pageNo = pageNo;
-    }
-
-    public String getSearchCode() {
-        return searchCode;
-    }
-
-    public void setSearchCode(String searchCode) {
-        this.searchCode = searchCode;
-    }
-
-    public Long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Long projectId) {
-        this.projectId = projectId;
-    }
+    private String deviceName;
 
 
 
-    @Override
-    public String toString() {
-        return "HlsCameraBO{" +
-                "projectId=" + projectId +
-                ", pageSize=" + pageSize +
-                ", pageNo=" + pageNo +
-                '}';
-    }
+
+
 }
