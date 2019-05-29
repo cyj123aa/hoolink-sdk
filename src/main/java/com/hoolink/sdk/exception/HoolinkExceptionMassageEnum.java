6caf045f8be081b6081771322e9ee8251d01022c
陈志镖 <1193381165@qq.com>
2019-05-29 09:07:11 +0800

    需求_610_任务_6547:部门资源的文件权限设置

--- src/main/java/com/hoolink/sdk/exception/HoolinkExceptionMassageEnum.java
+++ src/main/java/com/hoolink/sdk/exception/HoolinkExceptionMassageEnum.java
@@ -169,7 +169,8 @@ public enum HoolinkExceptionMassageEnum {
 
     /** edm */
     USER_SECURITY_NOT_EXIST("用户资源密保等级不存在"),
-    OUTPUT_FILE_NOT_EXIST("输出的文件不纯在")
+    OUTPUT_FILE_NOT_EXIST("输出的文件不纯在"),
+    REPERTORY_INFO_ERROR("需要设置的文件资源为空！")
     ;
 
     private String message;
