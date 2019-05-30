c700f4fd18712bbcfed928be3fa6c2e021fdd6f7
王栋 <305306733@qq.com>
2019-05-30 11:04:13 +0800

    需求_616_任务_6651:新增清空设备文件相关BO

---
+++ src/main/java/com/hoolink/sdk/bo/device/req/ScreenIdParamBO.java
@@ -0,0 +1,23 @@
+package com.hoolink.sdk.bo.device.req;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+/**
+ * @author wangdong
+ * @date 2019/5/29 20:11
+ */
+@Data
+public class ScreenIdParamBO implements Serializable {
+    private static final long serialVersionUID = 154587828461578876L;
+    
+    /*** 显示屏ID */
+    @NotNull(message = "显示屏ID不能为空")
+    private Long screenId;
+
+    /*** 项目ID */
+    @NotNull(message = "项目ID不能为空")
+    private Long projectId;
+}
