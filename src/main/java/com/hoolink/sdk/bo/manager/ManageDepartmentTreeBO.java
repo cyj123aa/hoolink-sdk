6c3dfcfa857fd44b9d87e00a4f87ced048572994
陈志镖 <1193381165@qq.com>
2019-05-30 17:29:39 +0800

    需求_610_任务_6668:组织架构树

---
+++ src/main/java/com/hoolink/sdk/bo/manager/ManageDepartmentTreeBO.java
@@ -0,0 +1,33 @@
+package com.hoolink.sdk.bo.manager;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @Author: tonghao
+ * @Date: 2019/5/29 19:29
+ */
+@Data
+public class ManageDepartmentTreeBO implements Serializable {
+
+    private static final long serialVersionUID = 7728059630076949882L;
+    /** 组织架构id */
+    private Long id;
+
+    /** 组织架构名称 */
+    private String name;
+
+    /** 组织类型 */
+    private Byte deptType;
+
+    /** 父节点id */
+    private Long parentId;
+
+    /*** 组织架构下的子节点*/
+    private List<ManageDepartmentTreeBO> childTreeList;
+
+    /** 组织架构下的人员 */
+    private List<SimpleDeptUserBO> userList;
+}
