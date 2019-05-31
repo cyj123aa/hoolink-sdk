9399001862678e0ca21ef88105b744b92a26911c
胡亚曦 <hyx199170@163.com>
2019-05-31 10:46:27 +0800

    提交代码

--- src/main/java/com/hoolink/sdk/bo/device/FaceHistoryBO.java
+++ src/main/java/com/hoolink/sdk/bo/device/FaceHistoryBO.java
@@ -3,12 +3,12 @@ package com.hoolink.sdk.bo.device;
 import lombok.Data;
 
 /**
- * @ClassName InspectionHistoryBO
+ * @ClassName FaceHistoryBO
  * @Author huyaxi
  * @DATE 2019/5/31 9:55
  */
 @Data
-public class InspectionHistoryBO {
+public class FaceHistoryBO {
     private Long projectId;
     private String faceDate;
     private Long bigObsId;
