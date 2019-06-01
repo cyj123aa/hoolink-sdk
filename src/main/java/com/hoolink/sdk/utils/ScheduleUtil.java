005f942580e683e53827c051e5fb54818667fcf6
许保锋 <yanwu0527@163.com>
2019-06-01 16:05:55 +0800

    提交: 1.显示屏定时任务根据时间更新;

---
+++ src/main/java/com/hoolink/sdk/utils/ScheduleUtil.java
@@ -0,0 +1,40 @@
+package com.hoolink.sdk.utils;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
+
+/**
+ * @author <a herf="mailto:yanwu0527@163.com">XuBaofeng</a>
+ * @date 2019-06-01 15:42.
+ * <p>
+ * description:
+ */
+public class ScheduleUtil {
+    private static final String TEMP = " ";
+
+    public static String assemblyCron(LocalTime time) {
+        return assemblyCron(time, null);
+    }
+
+    /**
+     * 组装corn
+     *
+     * @param time
+     * @return
+     */
+    public static String assemblyCron(LocalTime time, LocalDate date) {
+        if (Objects.isNull(time)) {
+            time = LocalTime.now();
+        }
+        if (Objects.isNull(date)) {
+            date = LocalDate.now();
+        }
+        return time.getSecond() + TEMP +
+                time.getMinute() + TEMP +
+                time.getHour() + TEMP +
+                date.getDayOfMonth() + TEMP +
+                date.getMonthValue() + TEMP +
+                date.getYear();
+    }
+}
