e7ec1ad8318a1644a73693a2a17b8bdb3e5bb35c
徐力 <462120096@qq.com>
2019-06-03 20:55:21 +0800

    edm提交

--- src/main/java/com/hoolink/sdk/exception/HoolinkExceptionMassageEnum.java
+++ src/main/java/com/hoolink/sdk/exception/HoolinkExceptionMassageEnum.java
@@ -175,6 +175,7 @@ public enum HoolinkExceptionMassageEnum {
     OUTPUT_FILE_NOT_EXIST("输出的文件不纯在"),
     REPERTORY_INFO_ERROR("需要设置的文件资源为空！"),
     ORG_LIST_TREE_ERROR("获取组织架构树失败！"),
+    FILE_NO_JURISDICTION("获取文件密保权限失败"),
     MANAGE_DEPARTMENT_TREE("获取组织架构失败")
     ;
 
