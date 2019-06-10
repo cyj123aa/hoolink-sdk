9551a6fd9e4af51de25e1521ab20e0f5407e6e0c
许保锋 <yanwu0527@163.com>
2019-06-10 17:49:14 +0800

    提交: BO定义

---
+++ src/main/java/com/hoolink/sdk/bo/alarm/LightAnalyzerBO.java
@@ -0,0 +1,14 @@
+package com.hoolink.sdk.bo.alarm;
+
+import lombok.Data;
+
+/**
+ * @author <a herf="mailto:yanwu0527@163.com">XuBaofeng</a>
+ * @date 2019-06-10 17:48.
+ * <p>
+ * description:
+ */
+@Data
+public class LightAnalyzerBO extends AnalyzerBO {
+    private static final long serialVersionUID = -47675939663507389L;
+}
---
+++ src/main/java/com/hoolink/sdk/bo/alarm/MonitorAnalyzerBO.java
@@ -0,0 +1,14 @@
+package com.hoolink.sdk.bo.alarm;
+
+import lombok.Data;
+
+/**
+ * @author <a herf="mailto:yanwu0527@163.com">XuBaofeng</a>
+ * @date 2019-06-10 17:47.
+ * <p>
+ * description:
+ */
+@Data
+public class MonitorAnalyzerBO extends AnalyzerBO {
+    private static final long serialVersionUID = 6825318401784818860L;
+}
