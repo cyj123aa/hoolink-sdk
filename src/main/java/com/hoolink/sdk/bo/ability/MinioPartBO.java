3a0be9a0cbd4e6cb1e093ee790803cd4db4d90e7
lijunling <294242871@qq.com>
2019-06-10 13:33:55 +0800

    minio

---
+++ src/main/java/com/hoolink/sdk/bo/ability/MinioPartBO.java
@@ -0,0 +1,21 @@
+package com.hoolink.sdk.bo.ability;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author lijunling
+ *
+ * 2019年6月10日-上午11:26:03
+ */
+@Data
+public class MinioPartBO {
+	private Integer partNumber;
+	private String etag;
+	
+	public MinioPartBO(Integer partNumber, String etag) {
+		super();
+		this.partNumber = partNumber;
+		this.etag = etag;
+	}
+}
