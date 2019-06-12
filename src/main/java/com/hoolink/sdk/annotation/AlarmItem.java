3c3fb213ef2460e2faf0ab25397f1b81a7218c31
张鑫 <threegold@foxmail.com>
2019-06-12 19:46:31 +0800

    完善单灯报警逻辑

--- src/main/java/com/hoolink/sdk/annotation/AlarmItem.java
+++ src/main/java/com/hoolink/sdk/annotation/AlarmItem.java
@@ -2,12 +2,16 @@ package com.hoolink.sdk.annotation;
 
 import com.hoolink.sdk.enums.alarm.ItemEnum;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * @author <a herf="mailto:yanwu0527@163.com">XuBaofeng</a>
  * @date 2019-06-12 13:55.
  * <p>
  * description:
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface AlarmItem {
 
     ItemEnum value();
