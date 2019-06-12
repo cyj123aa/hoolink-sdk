92a04d39444cf289ae3eb85138e25d05b792673d
许保锋 <yanwu0527@163.com>
2019-06-12 14:57:54 +0800

    提交: 告警解析器新增注解

---
+++ src/main/java/com/hoolink/sdk/annotation/AlarmItem.java
@@ -0,0 +1,15 @@
+package com.hoolink.sdk.annotation;
+
+import com.hoolink.sdk.enums.alarm.ItemEnum;
+
+/**
+ * @author <a herf="mailto:yanwu0527@163.com">XuBaofeng</a>
+ * @date 2019-06-12 13:55.
+ * <p>
+ * description:
+ */
+public @interface AlarmItem {
+
+    ItemEnum item();
+
+}
