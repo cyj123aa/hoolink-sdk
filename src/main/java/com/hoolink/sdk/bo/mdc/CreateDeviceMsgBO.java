ed811477ddfd1c9077fbc1248ed5431e01afb016
朱礼 <247085247@qq.com>
2019-06-13 17:56:39 +0800

    task_7172 消息中心-消息生成

--- src/main/java/com/hoolink/sdk/bo/mdc/CreateDeviceMsgBO.java
+++ src/main/java/com/hoolink/sdk/bo/mdc/CreateDeviceMsgBO.java
@@ -22,6 +22,12 @@ public class CreateDeviceMsgBO extends CreateMsgBaseBO implements Serializable {
     /** 消息类型 */
     private Long typeId;
 
+    /** 阈值上界*/
+    private Double upperBound;
+
+    /** 阈值下界*/
+    private Double lowerBound;
+
     /** 故障参数 */
     private List<MessageRecordDetailBO> messageRecordDetailBOList;
 }
