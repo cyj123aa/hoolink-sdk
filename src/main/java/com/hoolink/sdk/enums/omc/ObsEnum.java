59003d6f139e211e9a4c9fa72ab55d8b7e16d6f2
陈岳军 <491199291@qq.com>
2019-06-17 19:32:11 +0800

    运维中心代码完善

--- src/main/java/com/hoolink/sdk/enums/omc/ObsEnum.java
+++ src/main/java/com/hoolink/sdk/enums/omc/ObsEnum.java
@@ -8,39 +8,39 @@ public enum ObsEnum {
     /**
      *创建工单
      */
-    CREATEWORK(1, "创建工单"),
+    CREATEWORK((byte)1, "创建工单"),
     /**
      *
      */
-    TRANSFER(2, "申请转派"),
+    TRANSFER((byte)2, "申请转派"),
     /**
      *
      */
-    ADDRECEIPT(3, "添加回执"),
+    ADDRECEIPT((byte)3, "添加回执"),
     /**
      *
      */
-    DELAYWORK(4, "申请延期"),
+    DELAYWORK((byte)4, "申请延期"),
     /**
      *
      */
-    CANTCOMPLETEWORK(5, "无法完成"),
+    CANTCOMPLETEWORK((byte)5, "无法完成"),
     /**
      *
      */
-    COMPLETEWORK(6, "完成工单"),
+    COMPLETEWORK((byte)6, "完成工单"),
     /**
      *
      */
-    REVIEW(7, "审批"),
+    REVIEW((byte)7, "审批"),
     ;
 
 
-    public Integer key;
+    public Byte key;
     public String value;
 
 
-    ObsEnum(Integer key, String value) {
+    ObsEnum(Byte key, String value) {
         this.key = key;
         this.value = value;
     }
--- src/main/java/com/hoolink/sdk/enums/omc/WorkStateEnum.java
+++ src/main/java/com/hoolink/sdk/enums/omc/WorkStateEnum.java
@@ -6,25 +6,25 @@ package com.hoolink.sdk.enums.omc;
 
 public enum WorkStateEnum {
 
-    COMPLETE(1,"处理完成"),
+    COMPLETE((byte)1,"处理完成"),
 
-    CANCEL(2,"无法完成"),
+    CANCEL((byte)2,"无法完成"),
 
-    APPROVAL(3,"指派工单"),
-    TRANSFER(4,"申请转派"),
-    DELAY_WORK(5,"申请延期"),
-    TURN_DOWN(6,"驳回"),
-    CREATE(7,"创建工单"),
+    APPROVAL((byte)3,"指派工单"),
+    TRANSFER((byte)4,"申请转派"),
+    DELAY_WORK((byte)5,"申请延期"),
+    TURN_DOWN((byte)6,"驳回"),
+    CREATE((byte)7,"创建工单"),
 
-    COMPLETE_REVIEW(8,"完成审核通过"),
-    CANCEL_REVIEW(9,"无法完成审核通过"),
-    PERFECT(10,"完善工单"),
+    COMPLETE_REVIEW((byte)8,"完成审核通过"),
+    CANCEL_REVIEW((byte)9,"无法完成审核通过"),
+    PERFECT((byte)10,"完善工单"),
     ;
 
-    public Integer key;
+    public Byte key;
     public String value;
 
-    WorkStateEnum(Integer key, String value) {
+    WorkStateEnum(Byte key, String value) {
         this.key = key;
         this.value = value;
     }
