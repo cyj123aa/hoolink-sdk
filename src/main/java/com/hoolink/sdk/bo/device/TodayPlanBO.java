334bb48edce5ee11465c7264dbd1764e569c12a3
hoolink_zhouyun <492725557@qq.com>
2019-06-20 10:59:15 +0800

    今日运行单灯

---
+++ src/main/java/com/hoolink/sdk/bo/device/TodayPlanBO.java
@@ -0,0 +1,44 @@
+package com.hoolink.sdk.bo.device;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author zhouyun
+ * @date 2019/6/18 14:33
+ * @desc
+ **/
+@Data
+public class TodayPlanBO implements Serializable {
+
+    private static final long serialVersionUID = 712550341806033316L;
+    /** 运行状态已完成常量 */
+    public static final Byte END = (byte)1;
+    /** 运行状态正在进行常量 */
+    public static final Byte STARTING = (byte)2;
+    /** 运行状态未开始常量 */
+    public static final Byte NON_START = (byte)3;
+
+    /** 设备类型 DeviceTypeEnum枚举 */
+    private Long deviceType;
+    /** 任务名称 (清单名称或者配置名称) */
+    private String taskName;
+    /** 设备数量 */
+    private Integer deviceNum;
+    /** 定时开始时间 */
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime startDateTime;
+    /** 定时结束时间 */
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime endDateTime;
+    /** 运行状态 (1:已完成 2:正在进行 3:未开始) */
+    private Byte runStatus;
+}
