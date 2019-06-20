eefafb9a20849487d2c1aae4684e5db0ca689fcf
李晓然 <wy424916@163.com>
2019-06-20 18:53:56 +0800

    删除文件的新增minioId

---
+++ src/main/java/com/hoolink/sdk/bo/edm/AbilityFileRemoveBO.java
@@ -0,0 +1,21 @@
+package com.hoolink.sdk.bo.edm;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Author by lixiaoran
+ * @date 2019-06-20
+ */
+@Data
+public class AbilityFileRemoveBO {
+    /**
+     * obsId
+     */
+    private List<Long> obsIds;
+    /**
+     * minioIds
+     */
+    private List<Long> minioIds;
+}
