103dde8aa592fea3cfd0627af49c412512bf007f
许保锋 <yanwu0527@163.com>
2019-07-03 16:01:48 +0800

    merge test into device-control

---
+++ src/main/java/com/hoolink/sdk/bo/edm/FileOutPutSengdMsgToMqBO.java
@@ -0,0 +1,13 @@
+package com.hoolink.sdk.bo.edm;
+
+import lombok.Data;
+
+/**
+ * @author chenzhixiong
+ * @date 2019/7/3 11:23
+ */
+@Data
+public class FileOutPutSengdMsgToMqBO {
+    private Long userId;
+
+}
