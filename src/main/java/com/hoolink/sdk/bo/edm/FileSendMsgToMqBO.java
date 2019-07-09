7c03e5ce6daec708cef27a2eb99092749f9bb7ea
chenzhixiong <1198395447@qq.com>
2019-07-09 10:14:05 +0800

    提交：websocket推送bo

---
+++ src/main/java/com/hoolink/sdk/bo/edm/FileSendMsgToMqBO.java
@@ -0,0 +1,13 @@
+package com.hoolink.sdk.bo.edm;
+
+import lombok.Data;
+
+/**
+ * @author chenzhixiong
+ * @date 2019/7/9 10:13
+ */
+@Data
+public class FileSendMsgToMqBO {
+  private String msg;
+  private Boolean flag;
+}
