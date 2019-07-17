8e57ea6d6bf7ad0fa19faead720ae8ad387840e3
陈岳军 <491199291@qq.com>
2019-07-17 13:49:23 +0800

    重启服务

--- src/main/java/com/hoolink/sdk/bo/omc/UserNodeBO.java
+++ src/main/java/com/hoolink/sdk/bo/omc/UserNodeBO.java
@@ -7,7 +7,11 @@ import lombok.Data;
  */
 @Data
 public class UserNodeBO {
+
+  private Long processId;
   private String processName;
 
   private String nodeName;
+
+  private String projectName;
 }
