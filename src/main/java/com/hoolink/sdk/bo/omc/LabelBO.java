d52f758c654f3836cb5ac8cc1373306988637d5e
陈岳军 <491199291@qq.com>
2019-07-25 17:16:18 +0800

    app 备注字段修改

--- src/main/java/com/hoolink/sdk/bo/omc/LabelBO.java
+++ src/main/java/com/hoolink/sdk/bo/omc/LabelBO.java
@@ -1,6 +1,7 @@
 package com.hoolink.sdk.bo.omc;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * @author chenyuejun
@@ -10,5 +11,6 @@ public class LabelBO {
     /**
      * 连接线中间文案
      */
+    @Length( max = 10, message = "节点线长度为0到10")
 private String text;
 }
