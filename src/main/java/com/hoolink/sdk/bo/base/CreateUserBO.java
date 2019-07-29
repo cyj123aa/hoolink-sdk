f3c546c676b612f04e4037a0ae115080f0b20249
张鑫 <threegold@foxmail.com>
2019-07-29 16:03:23 +0800

    bug_3722 根据需求修改支撑平台创建管理员默认值

--- src/main/java/com/hoolink/sdk/bo/base/CreateUserBO.java
+++ src/main/java/com/hoolink/sdk/bo/base/CreateUserBO.java
@@ -21,17 +21,14 @@ public class CreateUserBO implements Serializable {
     @NotBlank(message = "账号不允许为空", groups = {Group.Admin.class, Group.User.class})
     private String account;
     /*** 用户名称 */
-    @NotBlank(message = "姓名不允许为空", groups = {Group.Admin.class, Group.User.class})
+    @NotBlank(message = "姓名不允许为空", groups = {Group.User.class})
     private String name;
     /*** 用户性别 */
-    @NotNull(message = "性别不允许为空", groups = {Group.Admin.class, Group.User.class})
+    @NotNull(message = "性别不允许为空", groups = {Group.User.class})
     private Boolean sex;
     /*** 角色ID */
     @NotNull(message = "角色不允许为空", groups = {Group.User.class})
     private Long roleId;
-    /*** 手机号 */
-    @NotBlank(message = "手机号不允许为空", groups = {Group.Admin.class})
-    private String phone;
     /*** 客户号 */
     @NotBlank(message = "客户号不允许为空", groups = {Group.Admin.class})
     private String customerNo;
