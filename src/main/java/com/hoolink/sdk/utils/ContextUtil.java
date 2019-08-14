d3968daf5c1299b5a7d7229ed9ea4bb7d980493a
chenzhixiong <1198395447@qq.com>
2019-08-14 13:24:41 +0800

    提交：用户名称乱码

--- src/main/java/com/hoolink/sdk/utils/ContextUtil.java
+++ src/main/java/com/hoolink/sdk/utils/ContextUtil.java
@@ -60,7 +60,6 @@ public class ContextUtil {
             String user = context.getContext(ContextConstant.MANAGE_CURRENT_USER);
             if (StringUtils.isNotEmpty(user)) {
                 userBO = JSONUtils.parse(user, CurrentUserBO.class);
-                userBO.setUserName(URLDecoder.decode(userBO.getUserName(),"utf-8"));
             }
         } catch (Exception e) {
             throw new BusinessException(HoolinkExceptionMassageEnum.GET_CURRENT_USER_ERROR);
