a00ce4fcaa82a477555452589f2d9b86dae184a4
陈岳军 <491199291@qq.com>
2019-08-26 10:17:34 +0800

    long 转int 数据越界 先计算在转化

--- src/main/java/com/hoolink/sdk/utils/TimeUtil.java
+++ src/main/java/com/hoolink/sdk/utils/TimeUtil.java
@@ -70,9 +70,8 @@ public class TimeUtil {
      * @return
      */
     public static  Integer dilfferentDaysByMillisecond(Long start,Long end) {
-        Integer days = (int)(start - end) / (1000*3600*24) ;
+        Integer days = (int)((start - end) / (1000*3600*24)) ;
         return days;
     }
-
 
 }
