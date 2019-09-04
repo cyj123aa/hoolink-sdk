68a4eae35ac94b207458b767fa252c0243dc17af
许保锋 <yanwu0527@163.com>
2019-09-04 13:26:17 +0800

    提交：优化CRC-16/MODBUS算法[增加根据json对象\json字符串获取crc方法]

--- src/main/java/com/hoolink/sdk/utils/Crc16Util.java
+++ src/main/java/com/hoolink/sdk/utils/Crc16Util.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 /**
  * @author <a herf="mailto:yanwu0527@163.com">XuBaofeng</a>
  * @date 2019-08-26 14:22.
- * <p>
+ * <p/>
  * description:
  * 本方法使用CRC-16/MODBUS算法
  */
@@ -18,51 +18,134 @@ public class Crc16Util {
     private static final String SPACE = " ";
     private static final String ASCII = "US-ASCII";
 
+    private Crc16Util() {
+    }
+
     /**
-     * 根据报文byte数组，获取CRC-16 16进制字符串<p>
+     * 将16进制字符串进行高低位转换  <p/>
+     * 0xE647 >> 0x47E6         <p/>
+     * 0x484C01 >> 0x014C48     <p/>
+     * 0x722E696D >> 0x6D692E72
+     *
+     * @return
+     */
+    public static String convertHighLow(String str) {
+        if (StringUtils.isBlank(str)) {
+            throw new RuntimeException("The string cannot be empty!");
+        }
+        str = str.replaceAll(SPACE, NUL);
+        int strLen = str.length();
+        if ((strLen & ONE) == ONE) {
+            // ----- 报文字符串必须是以一个字节为单位（两个字符为一个字节），所以当去除所有空格后的字符串为单数时说明字符串错误
+            throw new RuntimeException("Incorrect String format!");
+        }
+        StringBuffer buffer = new StringBuffer();
+        for (int i = strLen; i > 0; i -= TWO) {
+            buffer.append(str.substring(i - TWO, i));
+        }
+        return buffer.toString();
+    }
+
+    /**
+     * 根据报文json对象，获取CRC-16 16进制字符串<p/>
+     * {"code":"setServerAddr","data":{"type":2,"inAddr":"www.hoolink.com"},"sn":"light001","type":"light","version":"010101","seq":6}
+     * >> 0xB84B
+     *
+     * @param data json对象
+     * @return CRC值（16进制）
+     */
+    public static <T> String getCrc16HexStrByJson(T data) throws Exception {
+        return crc16ToHexStr(getCrc16ByJson(data));
+    }
+
+    /**
+     * 根据报文json对象，获取CRC-16 int值<p/>
+     * "{\"code\":\"setServerAddr\",\"data\":{\"type\":2,\"inAddr\":\"www.hoolink.com\"},\"sn\":\"light001\",\"type\":\"light\",\"version\":\"010101\",\"seq\":6}"
+     * >> 47179
+     *
+     * @param data json对象
+     * @return CRC值（10进制）
+     */
+    public static <T> int getCrc16ByJson(T data) throws Exception {
+        if (data == null) {
+            throw new RuntimeException("The data cannot be empty!");
+        }
+        return getCrc16ByJson(JSONUtils.toJSONString(data));
+    }
+
+    /**
+     * 根据报文json字符串，获取CRC-16 16进制字符串<p/>
+     * "{\"code\":\"setServerAddr\",\"data\":{\"type\":2,\"inAddr\":\"www.cloud-hoolink.com\"},\"sn\":\"1001126417824781234\",\"type\":\"light\",\"version\":\"010101\",\"seq\":62235}"
+     * >> 0xB84B
+     *
+     * @param data json字符串
+     * @return CRC值（16进制）
+     */
+    public static String getCrc16HexStrByJson(String data) throws Exception {
+        return crc16ToHexStr(getCrc16ByJson(data));
+    }
+
+    /**
+     * 根据报文json对象字符串，获取CRC-16 int值<p/>
+     * "{\"code\":\"setServerAddr\",\"data\":{\"type\":2,\"inAddr\":\"www.cloud-hoolink.com\"},\"sn\":\"1001126417824781234\",\"type\":\"light\",\"version\":\"010101\",\"seq\":62235}"
+     * >> 47179
+     *
+     * @param data json字符串
+     * @return CRC值（10进制）
+     */
+    public static int getCrc16ByJson(String data) throws Exception {
+        if (StringUtils.isBlank(data)) {
+            throw new RuntimeException("The string cannot be empty!");
+        }
+        data = data.replaceAll(SPACE, NUL);
+        return getCrc16ByHex(data.getBytes(ASCII));
+    }
+
+    /**
+     * 根据报文byte数组，获取CRC-16 16进制字符串<p/>
      * 48 4C 01 00 01 00 00 05 00 00 >> 0xE647
      *
      * @param data 报文数组
      * @return CRC值（16进制）
      */
-    public static String getCrc16HexStr(String data) {
-        return crcToHexStr(getCrc16(data));
+    public static String getCrc16HexStrByHex(String data) {
+        return crc16ToHexStr(getCrc16ByHex(data));
     }
 
     /**
-     * 根据报文byte数组，获取CRC-16 int值<p>
+     * 根据报文byte数组，获取CRC-16 int值<p/>
      * 48 4C 01 00 01 00 00 05 00 00 >> 58951
      *
      * @param data 报文数组
      * @return CRC值（10进制）
      */
-    public static int getCrc16(String data) {
+    public static int getCrc16ByHex(String data) {
         if (StringUtils.isBlank(data)) {
             // ----- 校验：报文字符串不能为空，否则抛异常
             throw new RuntimeException("The string cannot be empty!");
         }
-        return getCrc16(hexStrToByteArr(data));
+        return getCrc16ByHex(hexStrToByteArr(data));
     }
 
     /**
-     * 根据报文byte数组，获取CRC-16 16进制字符串<p>
+     * 根据报文byte数组，获取CRC-16 16进制字符串<p/>
      * {0x48, 0x4C, 0x01, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x00} >> 0xE647
      *
      * @param data 报文数组
      * @return CRC值（16进制）
      */
-    public static String getCrc16HexStr(byte[] data) {
-        return crcToHexStr(getCrc16(data));
+    public static String getCrc16HexStrByHex(byte[] data) {
+        return crc16ToHexStr(getCrc16ByHex(data));
     }
 
     /**
-     * 根据报文byte数组，获取CRC-16 int值<p>
+     * 根据报文byte数组，获取CRC-16 int值<p/>
      * {0x48, 0x4C, 0x01, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x00} >> 58951
      *
      * @param data 报文数组
      * @return CRC值（10进制）
      */
-    public static int getCrc16(byte[] data) {
+    public static int getCrc16ByHex(byte[] data) {
         if (data.length == 0) {
             // ----- 校验：报文数组不能为空，否则抛异常
             throw new RuntimeException("The array cannot be empty!");
@@ -94,7 +177,7 @@ public class Crc16Util {
     }
 
     /**
-     * 将16进制字符串转换为16进制Byte数组<p>
+     * 将16进制字符串转换为16进制Byte数组<p/>
      * 48 4C 01 00 01 00 00 05 00 00 >> {0x48, 0x4C, 0x01, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x00}
      *
      * @param str 报文字符串
@@ -104,7 +187,7 @@ public class Crc16Util {
         str = str.replaceAll(SPACE, NUL);
         int strLen = str.length();
         if ((strLen & ONE) == ONE) {
-            // ----- 报文字符串必须是以一个字节为单位（两位为一个字节），所以当去除所有空格后的报文长度为单数时说明报文错误
+            // ----- 报文字符串必须是以一个字节为单位（两个字符为一个字节），所以当去除所有空格后的报文长度为单数时说明报文错误
             throw new RuntimeException("Incorrect message format!");
         }
         byte[] result = new byte[strLen / TWO];
@@ -117,22 +200,22 @@ public class Crc16Util {
     }
 
     /**
-     * 将CRC-16值转换成16进制字符串，且保持最小长度为4位<p>
+     * 将CRC-16值转换成16进制字符串，且保持最小长度为4位<p/>
      * 58951 >> E647
      *
      * @param data CRC值（10进制）
      * @return CRC值（16进制）
      */
-    private static String crcToHexStr(int data) {
+    private static String crc16ToHexStr(int data) {
         String crcStr = Integer.toHexString(data).toUpperCase();
         int size = 4 - crcStr.length();
-        StringBuilder builder = new StringBuilder();
+        StringBuffer buffer = new StringBuffer();
         // ---- 长度不够 4 位高位自动补0
         while (size > 0) {
-            builder.append("0");
+            buffer.append("0");
             size--;
         }
-        return builder.append(crcStr).toString();
+        return buffer.append(crcStr).toString();
     }
 
     /**
@@ -142,36 +225,45 @@ public class Crc16Util {
      */
     private static void printHexStr(String str) {
         String[] split = str.split(SPACE);
-        StringBuilder builder = new StringBuilder();
-        builder.append("    unsigned char arr[] = {");
+        System.out.println("    str: " + split.length + " -> " + str);
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("    unsigned char arr[] = {");
         for (int i = 0; i < split.length; i++) {
-            builder.append("0x").append(split[i]);
+            buffer.append("0x").append(split[i]);
             if (i < split.length - 1) {
-                builder.append(", ");
+                buffer.append(", ");
             }
         }
-        builder.append("};");
-        System.out.println(builder.toString());
+        buffer.append("};");
+        System.out.println(buffer.toString());
         System.out.println("    int len = " + split.length + ";");
     }
 
     /**
-     * 测试CRC获取
+     * 输出json字符串与长度, 提供给 C++ CRC校验方法 测试 代码使用
      *
-     * @param args
+     * @param str json字符串
      */
-    public static void main(String[] args) throws Exception {
-        String str = "48 4C 01 00 01 00 00 05 00 00";
-        // ----- 输出16进制数组给 C++ 测试使用
-        Crc16Util.printHexStr(str);
-        // ----- 获取CRC-16的值
-        System.out.println("crc16 int is: " + Crc16Util.getCrc16(str));
-        System.out.println("crc16 hex is: " + Crc16Util.getCrc16HexStr(str));
-        // ----- 获取FTP地址的十六进制数组
-        String ftp = "ftp://127.0.0.1/xxx-dt1.1-v1.1.2.2r.img";
-        byte[] asc = ftp.getBytes(ASCII);
-        System.out.println(ByteUtil.bytesToHexPrint(asc));
-        System.out.println(Integer.toHexString(asc.length));
+    private static void printJsonStr(String str) {
+        str = str.replaceAll(SPACE, NUL);
+        System.out.println("    str: " + str.length() + " -> " + str);
+        char[] chars = str.toCharArray();
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("    unsigned char arr[] = {");
+        for (int i = 0; i < chars.length; i++) {
+            char item = chars[i];
+            buffer.append("\'");
+            if (item == '\"') {
+                buffer.append("\\");
+            }
+            buffer.append(item).append("\'");
+            if (i < chars.length - 1) {
+                buffer.append(", ");
+            }
+        }
+        buffer.append("};");
+        System.out.println(buffer.toString());
+        System.out.println("    int len = " + chars.length + ";");
     }
 
 }
