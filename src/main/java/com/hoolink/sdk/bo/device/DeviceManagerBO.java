9cd39fbbb217f973f42bdc5840dadad7efb9854e
张鑫 <threegold@foxmail.com>
2019-09-17 11:54:20 +0800

    修复告警与阈值事件无法区分的问题

--- src/main/java/com/hoolink/sdk/bo/device/DeviceManagerBO.java
+++ src/main/java/com/hoolink/sdk/bo/device/DeviceManagerBO.java
@@ -69,6 +69,9 @@ public class DeviceManagerBO implements Serializable {
     /*** 异常状态 */
     private Boolean alarmStatus;
 
+    /*** 事件状态 */
+    private Boolean eventStatus;
+
     /*** 描述 */
     private String description;
 
